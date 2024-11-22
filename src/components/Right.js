cb73abf1b3e8026c8b476cd9118166c4bda16aaa
Akshat Choudhary <ac822023@student.nitw.ac.in>
2024-11-22 11:24:13 +0530

    Right.js

--- src/components/Right.js
+++ src/components/Right.js
@@ -18,7 +18,7 @@ const Right = ({ config }) => {
     const link = document.createElement("a");
     link.href = url;
   
-    // Use configName as the filename, default to "config.json" if configName is empty
+    
     const fileName = config.configName ? `${config.configName}.json` : "config.json";
     link.download = fileName;
   
@@ -31,10 +31,10 @@ const Right = ({ config }) => {
 
   return (
     <div className="w-1/2 h-screen p-6 bg-gray-50 relative flex flex-col">
-      {/* Live Preview Title */}
+      
       <h2 className="text-2xl font-bold mb-4">Live Preview</h2>
 
-      {/* Centered Card */}
+      
       <div className="flex-grow flex items-center justify-center">
         <div
           className="rounded-2xl shadow-md flex flex-col items-center border-full"
@@ -43,7 +43,7 @@ const Right = ({ config }) => {
             fontFamily: config.fontFamily,
           }}
         >
-          {/* Chat Header */}
+          
           <div
             className="flex items-center justify-between px-4 py-2 rounded-t w-full "
             style={{
@@ -57,14 +57,14 @@ const Right = ({ config }) => {
             <button className="text-white font-bold">&times;</button>
           </div>
 
-          {/* Chat Body */}
+          
           <div className="p-4 text-sm w-full pb-60 flex items-center" style={{ color: config.chatFontColor }}>
               <img src={config.avatarImage} alt="Avatar" className="w-8 h-8 rounded-full mr-2"/>
               <span>Hi! I'm {config.botName}, your friendly concierge. How can I help?</span>
           </div>
 
 
-          {/* Chat Footer */}
+          
           <div className="py-3 border-b text-center text-xs text-gray-600 w-full bg-white">
             Need help? Just type or say it.
             <img src={config.launcherImage} alt="Launcher" className="absolute bottom-48 right-72 w-16 h-16 rounded-lg"/>
@@ -75,7 +75,7 @@ const Right = ({ config }) => {
       </div>
       
 
-      {/* Download Config Button */}
+      
       <button
         onClick={handleDownloadConfig}
         className="absolute bottom-6 px-4 py-2 bg-black text-white rounded-lg shadow hover:bg-gray-800"
